9ddc4b994ee4086e34a57cd851f29fff68e45ae9
Zolika1351 <zolika1351@mail.com>
2021-09-19 17:40:30 +0200

    use SetVelocity in pad example

--- examples/PadExample/source/dllmain.cpp
+++ examples/PadExample/source/dllmain.cpp
@@ -8,10 +8,12 @@ void PadExample1()
 	{
 		auto pad = ped->GetPadFromPlayer(); // can also use CPad::GetPad()
 		int value = pad->m_aValues[INPUT_JUMP].m_nCurrentValue;
-		// while SET_CHAR_VELOCITY would be better suited for this, the buggy velocity by using the collider is too good
-		if (auto collider = ped->GetConstrainedCollider())
+		if (ped->m_pInstGta->m_nRigidBodyIndex != -1)
 		{
-			collider->m_vPedVelocity.z += CTimer::ms_fTimeStep * value * 100;
+			CVector v;
+			ped->GetVelocity(&v);
+			v.z += CTimer::ms_fTimeStep * value * 10;
+			ped->GetConstrainedCollider()->SetVelocity(&v);
 		}
 	}
 }
