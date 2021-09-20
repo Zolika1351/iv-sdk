6454da09b1cfdbff50752d701db21449ebdcbe3b
Zolika1351 <zolika1351@mail.com>
2021-09-20 19:19:56 +0200

    ped spawning example with CPedFactoryNY

---
+++ examples/SpawnPedExample/source/dllmain.cpp
@@ -0,0 +1,31 @@
+#include "../../../include/IVSDK.cpp"
+
+// every frame while in-game
+void plugin::processScriptsEvent()
+{
+	// spawn m_y_multiplayer if L is pressed
+	if (Scripting::IS_GAME_KEYBOARD_KEY_JUST_PRESSED(KEY_L))
+	{
+		int index;
+		auto mdl = CModelInfo::GetModelInfo(rage::atStringHash("m_y_multiplayer"), &index);
+		CStreaming::ScriptRequestModel(rage::atStringHash("m_y_multiplayer"));
+		CStreaming::LoadAllRequestedModels(0);
+		tUnkSpawnData data = { 0, 0 };
+		CMatrix mat = *FindPlayerPed()->m_pMatrix;
+		mat.pos.x += 2;
+		CPed* ped = PedFactory->CreatePed(&data, index, &mat, 1, 1);
+		CWorld::Add(ped, 0);
+	}
+}
+
+// basically just DllMain but fancier and with the sdk initialized
+void plugin::gameStartupEvent()
+{
+
+}
+
+// right after gta.dat loads, put any extra loading related things here
+void plugin::gameLoadEvent()
+{
+
+}
