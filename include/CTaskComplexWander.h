30bb232853815505d74cffa3b97edcf0e02312c7
Zolika1351 <zolika1351@mail.com>
2021-09-20 16:48:15 +0200

    more CTask, fixups

--- include/CTaskComplexWander.h
+++ include/CTaskComplexWander.h
@@ -1,10 +1,10 @@
-class CTaskComplexWander : public CTask
+class CTaskComplexWander : public CTaskComplex
 {
 public:
 	uint8_t pad[0x80];											// 00-80
 	float m_fUnk;												// 80-84 set to 0 upon initializing CTaskComplexWanderStandard
 
-	CTaskComplexWander(uint32_t moveState, float initialDir, bool bSensibly, float targetRadius, uint32_t unk1)
+	void New(uint32_t moveState, float initialDir, bool bSensibly, float targetRadius, uint32_t unk1)
 	{
 		((void(__thiscall*)(CTaskComplexWander*, uint32_t, float, bool, float, uint32_t))(AddressSetter::Get(0x618C70, 0x7FA900)))(this, moveState, initialDir, bSensibly, targetRadius, unk1);
 	}
@@ -13,11 +13,11 @@ public:
 class CTaskComplexWanderStandard : public CTaskComplexWander
 {
 public:
-	CTaskComplexWanderStandard(uint32_t moveState, float initialDir, bool bSensibly, float targetRadius, uint32_t unk1) : CTaskComplexWander(moveState, initialDir, bSensibly, targetRadius, unk1)
+	void New(uint32_t moveState, float initialDir, bool bSensibly, float targetRadius, uint32_t unk1)
 	{
-		// set vftable
-		*(uint32_t*)this = AddressSetter::Get(0x969794, 0x995474);
-
+		((void(__thiscall*)(CTaskComplexWander*, uint32_t, float, bool, float, uint32_t))(AddressSetter::Get(0x618C70, 0x7FA900)))(this, moveState, initialDir, bSensibly, targetRadius, unk1);
+		// this was probably inlined
+		*(uint32_t*)this = AddressSetter::Get(0x969794, 0x995474); // set vftable
 		this->m_fUnk = 0;
 	}
 };
