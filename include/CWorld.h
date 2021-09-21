bf0fb8850b18393ef6f9001c83a5f5f860ad1240
Zolika1351 <zolika1351@mail.com>
2021-09-21 19:13:55 +0200

    CWorld::ProcessLineOfSight

--- include/CWorld.h
+++ include/CWorld.h
@@ -1,27 +1,36 @@
 class CEntity;
+class phInstGta;
 
-// todo: look into this, don't even know the exact size currently
 struct tLineOfSightResults
 {
-	uint32_t m_unk0_1 = 0;
-	uint32_t m_unkdword0_1;
-	uint32_t m_unkdword4_2;
-	uint32_t m_unkdword8_3;
-	uint32_t m_unkdword0_4;
-	uint32_t m_unkdword4_5;
-	uint32_t m_unkdword8_6;
-	uint32_t m_unkdword0_7;
-	uint32_t m_unkdword4_8;
-	uint32_t m_unkdword8_9;
-	uint32_t m_unk0_2 = 0;
-	uint32_t m_unk0_3 = 0;
-	uint32_t m_unk0_4 = 0;
-	int32_t m_unkneg1_1 = -1;
-	uint32_t m_unk0_5 = 0;
-	uint32_t m_unk0_6 = 0;
-	uint32_t m_unk0_7 = 0;
+	phInstGta* m_pInst = nullptr;						// 00-04 phInstGta pointer of hit entity
+	uint8_t pad[0xC];									// 04-10
+	CVector m_vEndPosition;								// 10-1C where it ended up
+	uint32_t m_nUnkFlags;								// 1C-20 0x11
+	CVector m_vUnk;										// 20-2C
+	uint32_t m_nUnkFlags2;								// 2C-30 0x11
+	CVector m_vUnk2;									// 30-3C
+	uint32_t m_nUnkFlags3;								// 3C-40 0x11
+	float m_fUnk1;										// 40-44
+	float m_fUnk2;										// 44-48
+	uint32_t m_nUnkFlags4;								// 48-4C 0x4F
+	uint32_t m_nUnkFlags5;								// 4C-50 0x20000
+	uint32_t m_nUnkFlags6;								// 50-54 0x10001
+};
+VALIDATE_SIZE(tLineOfSightResults, 0x54);
+VALIDATE_OFFSET(tLineOfSightResults, m_pInst, 0x0);
+VALIDATE_OFFSET(tLineOfSightResults, m_vEndPosition, 0x10);
+VALIDATE_OFFSET(tLineOfSightResults, m_vUnk, 0x20);
+VALIDATE_OFFSET(tLineOfSightResults, m_vUnk2, 0x30);
 
-	uint8_t pad[0x40];
+enum eLineOfSightFlags
+{
+	STATIC_COLLISION = 2,
+	BUILDINGS = 4,
+	VEHICLES = 8,
+	PEDS_BOUNDING_BOX = 32,
+	PEDS_COLLISION = 64,
+	OBJECTS = 128,
 };
 
 class CWorld
@@ -55,9 +64,11 @@ public:
 		((void(__cdecl*)())(AddressSetter::Get(0x419750, 0x3CEC20)))();
 	}
 
-	static bool ProcessLineOfSight(CVector* source, CVector* target, uint32_t* pUnk, tLineOfSightResults* pResults, uint32_t nFlags, uint32_t nUnk1, uint32_t nUnk2, uint32_t nUnk3, uint32_t nUnk4)
+	static bool ProcessLineOfSight(CVector* source, CVector* target, uint32_t* pUnk, tLineOfSightResults* pResults, uint32_t nFlags, uint32_t nUnk1, uint32_t nUnk2, uint32_t nSeeThroughShootThrough, uint32_t nUnk4)
 	{
 		// zmenu uses CWorld::ProcessLineOfSight(v, v2, 0, &ret, 142, 1, 0, 2, 4)
-		return ((uint32_t(__cdecl*)(CVector*, CVector*, uint32_t*, tLineOfSightResults*, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t))(AddressSetter::Get(0x596D80, 0x52A400)))(source, target, pUnk, pResults, nFlags, nUnk1, nUnk2, nUnk3, nUnk4);
+		// see/shoot 1 -> do see through check
+		// see/shoot 2 -> do shoot through check
+		return ((uint32_t(__cdecl*)(CVector*, CVector*, uint32_t*, tLineOfSightResults*, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t))(AddressSetter::Get(0x596D80, 0x52A400)))(source, target, pUnk, pResults, nFlags, nUnk1, nUnk2, nSeeThroughShootThrough, nUnk4);
 	}
 };
